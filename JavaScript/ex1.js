c6c54c2bd4d5515f87794f2a44c975a9daa934c6
agarciadona <72155549+agarciadona@users.noreply.github.com>
2022-05-31 14:46:00 +0200

    Add files via upload

---
+++ JavaScript/ex1.js
@@ -0,0 +1,11 @@
+
+let num = prompt("Dame el valor");
+
+let cuad = num ** 2;
+let cubo =  num ** 3;
+
+
+console.log(num+" elevado al cuadrado es " +parseInt(cuad)+".");
+console.log(num+" elevado al cubo es " +parseInt(cubo)+".");
+alert(num+" elevado al cuadrado es " +parseInt(cuad)+".")
+alert(num+" elevado al cubo es " +parseInt(cubo)+".");
---
+++ JavaScript/ex2.js
@@ -0,0 +1,11 @@
+
+
+let num1 = parseInt(prompt("Dame un numero"));
+let num2 = parseInt(prompt("Dame un segundo numero"));
+let num3 = parseInt(prompt("Dame un tercer numero"));
+let num4 = parseInt(prompt("Dame un cuarto numero "));
+let num5 = parseInt(prompt("Dame un quinto numero y hago la media"));
+
+let media = parseInt((num1+num2+num3+num4+num5)/5);
+alert("La media es "+media+".");
+
